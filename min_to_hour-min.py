068cf4e6ac86e0c8622b3d9dcb8c6928f45d8e87
FerrisLo <ferrislo@yahoo.com>
2020-06-12 20:19:46 +0800

    123 mins to hours and minutes

---
+++ min_to_hour-min.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Convert minutes to hours and minutes.
+
+minutes_to_convert = 123
+hours_decimal = minutes_to_convert / 60
+hours_part = int(hours_decimal)
+minutes_part = minutes_to_convert % 60
+
+print("Hours")
+print(hours_part)
+print("Minutes")
+print(minutes_part)
+
+
+
+
+
+
+
+
+
+
+
+
+
